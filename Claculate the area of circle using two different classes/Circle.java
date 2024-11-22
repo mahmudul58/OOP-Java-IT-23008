249e85d7b34a9ded71f5067705a74c14d06859d0
mahmudul <164454497+mahmudul58@users.noreply.github.com>
2024-11-22 18:39:59 +0600

    Add files via upload

---
+++ Claculate the area of circle using two different classes/Circle.java
@@ -0,0 +1,17 @@
+//Mahmudul(IT23008)
+
+class Circle {
+    private double radius; 
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+    public double getRadius() {
+        return radius;
+    }
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+}
